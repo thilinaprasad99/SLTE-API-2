eb02412d703ce337770f8f415d8da898f01e48c1
Thilina Disanayaka <you@example.com>
2021-11-22 09:42:00 +0530

    co

--- migrations/20211025123148-create-posts.js
+++ migrations/20211025123148-create-posts.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.TEXT('long')
       },
       description: {
         type: Sequelize.TEXT('long')
