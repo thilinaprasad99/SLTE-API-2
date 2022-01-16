a8d1f484e1ac5533bb5962008f7b5a93d1f6bce3
Thilina Disanayaka <dmtpkc@gmail.com>
2022-01-16 19:00:28 +0530

    duplicate results flow handled

--- src/services/Paper.js
+++ src/services/Paper.js
@@ -5,20 +5,29 @@ module.exports = {
   addResult: async (info, callback) => {
     const { USER_NAME, USER_EMAIL, INDEX_NUMBER, sp, ps, psp, qt, fut } = info;
     try {
-      models.Results.create({
-        user_name: USER_NAME,
-        user_email: USER_EMAIL,
-        index_number: parseInt(INDEX_NUMBER),
-        score: sp,
-        passing_score: ps,
-        passing_score_in_percentage: psp,
-        quiz_title: qt,
-        time_used: fut,
+      const data = await models.Results.findOne({
+        where: { quiz_title: qt, index_number: parseInt(INDEX_NUMBER) },
       });
-      callback({
-        statusCode: Constants.errorStatus.SUCCESS,
-        body: null,
-      });
+      if (!data) {
+        await models.Results.create({
+          user_name: USER_NAME,
+          user_email: USER_EMAIL,
+          index_number: parseInt(INDEX_NUMBER),
+          score: sp,
+          passing_score: ps,
+          passing_score_in_percentage: psp,
+          quiz_title: qt,
+          time_used: fut,
+        });
+        callback({
+          statusCode: Constants.errorStatus.SUCCESS,
+          body: null,
+        });
+      } else {
+        callback({
+          statusCode: Constants.errorStatus.SUCCESS,
+        });
+      }
     } catch (error) {
       callback({
         statusCode: Constants.errorStatus.SERVER_ERROR,
